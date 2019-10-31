4cebb2a5eb16a78ec3efdd10e1d5e0cc0ba4dfb4
David Kim <davidkim310@gmail.com>
2019-10-31 12:25:01 -0700

    removes logs

--- src/app/main/components/landing/landing.component.ts
+++ src/app/main/components/landing/landing.component.ts
@@ -17,15 +17,11 @@ export class LandingComponent implements OnInit {
     this.dataService.requestData().subscribe((res) => {
       this.ages = res[0];
       this.names = res[1];
-      console.log('receiving api', res);
       const finalArray = {};
       for (let i = 0; i < this.ages.length; i++) {
         finalArray[this.ages[i].id] = this.ages[i];
-        console.log('finalArray', finalArray);
       }
       for (let j = 0; j < this.names.length; j++) {
-        console.log('inside loop', this.names[j].id);
-        console.log('hasProp', finalArray.hasOwnProperty(this.names[j].id));
         if (finalArray.hasOwnProperty(this.names[j].id)) {
           finalArray[this.names[j].id].firstName = (this.names[j].firstName) ? this.names[j].firstName : '';
           finalArray[this.names[j].id].lastName = (this.names[j].lastName) ? (this.names[j].lastName) : '';
